814661c9c96009f273cd1f0ebd8fb5155b48536d
Mustafa Turan <mustafaturan.net@gmail.com>
2018-02-24 12:38:11 -0800

    Allow subscription to multiple topics Update dependency Fix tests

--- test/sse/server_test.exs
+++ test/sse/server_test.exs
@@ -1,5 +1,5 @@
 defmodule SSE.ServerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   use Plug.Test
   use SSE.ConnCase
   alias SSE.{Chunk, Server}
@@ -19,6 +19,17 @@ defmodule SSE.ServerTest do
 
     chunk = %Chunk{data: "Hi there!"}
     conn = Server.stream(conn, {@topic, chunk})
+
+    refute is_nil(Process.get(:timer_ref))
+    assert conn.state == :chunked
+    assert conn.status == 200
+  end
+
+  test "stream multiple topics", %{conn: conn} do
+    Process.send_after(self(), {:close}, 300)
+
+    chunk = %Chunk{data: "Hi there!"}
+    conn = Server.stream(conn, {[@topic, :another_event_occured], chunk})
 
     refute is_nil(Process.get(:timer_ref))
     assert conn.state == :chunked
@@ -46,24 +57,24 @@ defmodule SSE.ServerTest do
 
   test "stream and send a new event chunk", %{conn: conn} do
     pid = spawn(fn -> stream_chunk(conn) end)
-    event_watcher_id = Process.whereis EventBus.Watcher
+    event_watcher_id = Process.whereis EventBus.Manager.Observation
 
     :erlang.trace(pid, true, [:receive])
     :erlang.trace(event_watcher_id, true, [:receive])
+
+    Process.sleep(1000)
 
     chunk = %Chunk{data: "Hi again!"}
     event = %EventBus.Model.Event{id: 1, data: chunk, topic: @topic}
     EventBus.notify(event)
 
-    Process.sleep(3000)
-    Process.send_after(pid, {:close}, 6000)
-
-    assert_received {:trace, ^pid, :receive, {:sse, :test_sse_sent, _}}
-    assert_received {:trace, ^event_watcher_id, :receive,
-      {:"$gen_cast", {:mark_as_completed, _}}}
+    Process.send_after(pid, {:close}, 3000)
+    assert_receive {:trace, ^pid, :receive, {:sse, :test_sse_sent, _}}, 3000
+    assert_receive {:trace, ^event_watcher_id, :receive,
+      {:"$gen_cast", {:mark_as_completed, _}}}, 3000
   end
 
-  def stream_chunk(conn) do
+  defp stream_chunk(conn) do
     chunk = %Chunk{data: "test sse event data"}
     Server.stream(conn, {:test_sse_sent, chunk})
   end
