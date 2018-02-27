107ff014a5606ba179d902ce20fa3742dbccc06b
Mustafa Turan <mustafaturan.net@gmail.com>
2018-02-26 18:26:02 -0800

    Bump patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SSE.MixProject do
   def project do
     [
       app: :sse,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
