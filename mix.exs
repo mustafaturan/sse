3c40056a05311a9f0eee6717944c7f2f402b5d5b
Mustafa Turan <mustafaturan.net@gmail.com>
2018-12-03 20:58:57 -0800

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SSE.MixProject do
   def project do
     [
       app: :sse,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
